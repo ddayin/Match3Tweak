5f111ec1cec14bd7ad8d232c3e710f079a1e49e2
ddayin <ddayin@hotmail.com>
2020-08-30 16:51:36 +0900

    first

---
+++ Source/UnrealMatch3.Target.cs
@@ -0,0 +1,24 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UnrealMatch3Target : TargetRules
+{
+	public UnrealMatch3Target(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		ExtraModuleNames.AddRange( new string[] { "Match3" } );
+
+        if (Target.Platform == UnrealTargetPlatform.IOS && !bIsEngineInstalled)
+        {
+            bCompileAPEX = false;
+            bCompileNvCloth = false;
+            bCompileICU = true;
+			bBuildDeveloperTools = false;
+            bCompileRecast = true;
+            bCompileFreeType = true;
+            bCompileForSize = true;
+        }
+	}
+}
